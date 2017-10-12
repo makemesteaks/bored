4a9cf9bbdaea003853ca31ccc2873b5d40b198e0
makemesteaks <castrofjoao@gmail.com>
2017-10-12 18:02:02 +0100

    gndaung

---
+++ Graphs/dfs.py
@@ -0,0 +1,29 @@
+# DICTIONARY WITH ALL THE DOTS AND CONNECTIONS
+
+d = {'A' : ['D','B','F'],
+     'B' : ['A'],
+     'F' : ['D', 'A'],
+     'D' : ['B'] }
+     
+def dfs(graph, start, path=[]):
+
+# THE INITIAL VARIABLE IS THE ELEMENT INSIDE THE LIST TO BE TESTED
+    initial = [start]
+
+	# LOOP UNTIL WE RUN OUT OF INITIAL POSSIBILITES
+    while initial:
+
+	# THIS IS THE VARIABLE TO BE CHECKED AGAINST THE PATH
+        check = initial.pop()
+        if check not in path:
+
+	# IF IT DOESN'T EXIST, ADD TO THE PATH
+            path.append(check)
+
+	# INITIAL JUMPS TO THE NEXT CHARACTER TO BE TESTED AND RESPECTIVE NODES
+            initial = graph[check]
+
+	# RETURNS FINAL PATH
+    return path
+    
+dfs(d, 'B')
